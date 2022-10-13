ba5379ab559190d839a2a64da8b34c8c1bbc5fd3
João Marcelo Cassavara <joao.cassavara@estudante.ifms.edu.br>
2022-10-13 15:00:40 -0400

    categoria_produto

--- resources/views/produto/index.blade.php
+++ resources/views/produto/index.blade.php
@@ -1,24 +1,33 @@
 @extends('layouts.app')
 
 @section('conteudo')
-<section class=" d-flex flex-wrap index_produto">
-    @forelse ($produtos as $produto)
-        <a class="m-2  card-link" href="{{ route('produto.visualizar', ['id' => $produto->id]) }}">
-            <div class="card shadow p-4">
+    <section class=" d-flex flex-wrap index_produto">
+        @forelse ($produtos as $produto)
+            <a class="m-4  card-link" href="{{ route('produto.visualizar', ['id' => $produto->id]) }}">
+                <div class="card shadow p-4">
 
-                <img src="{{Voyager::image( $produto->imagem) }}" width="245" height="245" alt="Imagem do produto">
+                    <img src="{{ Voyager::image($produto->imagem) }}" width="245" height="245" alt="Imagem do produto">
 
-                <div class="card-body text-center text-white">
-                    <h5 class="card-title">{{ $produto->nome }}</h5>
-                    <p class="card-text">{{ $produto->descricao }}</p>
-                    <p class="card-text">Preço${{ $produto->preco }} </p>
+                    <div class="card-body text-center text-white">
+                        <h5 class="card-title">{{ $produto->nome }}</h5>
+                        <p class="card-text">{{ $produto->descricao }}</p>
+                        <p class="card-text">Preço${{ $produto->preco }} </p>
+                    </div>
                 </div>
+            </a>
+
+        @empty
+            <p>Nenhum produto cadastrado para essa empresa</p>
+        @endforelse
+    </section>
+    <section class="Categoria_produto" >
+        <div class="card-categoria">
+            <img src="https://conteudo.imguol.com.br/c/entretenimento/fa/2019/04/18/legumes-e-verduras-1555620381375_v2_600x600.jpg.webp"
+                class="rounded-pill" alt="Imagem da categoria">
+            <div class="card-body text-center">
+                <h5 class="card-title">Verduras</h5>
+
             </div>
-        </a>
-
-    @empty
-        <p>Nenhum produto cadastrado para essa empresa</p>
-    @endforelse
-</section>
-
+        </div>
+    </section>
 @endsection
