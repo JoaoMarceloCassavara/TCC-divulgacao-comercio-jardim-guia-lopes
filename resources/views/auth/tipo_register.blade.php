992e9f828734268408ce64758f60f3a90ba0af3a
João Marcelo Cassavara <joao.cassavara@estudante.ifms.edu.br>
2022-12-30 11:55:30 -0400

    tipo_register_css

--- resources/views/auth/tipo_register.blade.php
+++ resources/views/auth/tipo_register.blade.php
@@ -2,36 +2,34 @@
 
 @section('conteudo')
     <div class="body-tipo-register ">
-               <section class=" index_padding-tipo-register">
-
-            <div class="tipo-register shadow p-3 mb-5 bg-body rounded">
+            <section class="tipo-register">
 
                 <header class="header_tipo_register">
-
                     <div class="text-center text_header_tipo_register">Deseja vender produtos, ou pedir produtos ?</div>
                 </header>
 
                 <div class="padding_bottom_deseja_pedir">
                     <div class="bottom_deseja_pedir">
                         <a class="btn btn-dark botao-pedir"href="{{route('register')}}">
-                            <h1 class="text-pedir">Deseja Pedir</h1>
+                            <h1 class="text-pedir">Deseja Pedir?</h1>
                         </a>
                     </div>
                 </div>
-                <div class="ou_tipo_register">
-                    <p class="ou_tipo_register_text">OU</p>
-                </div>
+
+
+                    <h6 class="ou_tipo_register_text">OU</h6>
+
+
                 <div class="bottom_deseja_vender">
                     <a class="btn btn-dark botao-pedir"href="{{route('auth.registrar_empresario')}}">
                         <h1 class="text-pedir">Deseja Vender?</h1>
                     </a>
+
+                    <h6 class="tipo_register-cidade">Guia Lopes da Laguna ou Jardim</h6>
+
                 </div>
-
-
-
-
             </div>
-    </div></section>
+        </section>
 
 
 @endsection
