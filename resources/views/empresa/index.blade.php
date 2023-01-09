e5de1b85390f6634b835dc524695d477b147667b
João Marcelo Cassavara <joao.cassavara@estudante.ifms.edu.br>
2023-01-09 17:15:43 -0400

    empresa_css

--- resources/views/empresa/index.blade.php
+++ resources/views/empresa/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('conteudo')
-    <section class=" d-flex index_padding">
+    <header class=" d-flex index_padding">
 
         @forelse ($categoria_empresas as $categoria_empresa)
             <a class="m-4 marcacao_a_remov " href="{{ route('empresa.categoria', ['id' => $categoria_empresa->id]) }}">
@@ -20,26 +20,28 @@
         @empty
             <p>Nenhuma Categoria Empresa cadastrado</p>
         @endforelse
-    </section>
+        </header>
     <section class=" d-flex flex-wrap index_empresa">
         @forelse ($empresas as $empresa)
-            <a class="m-2 marcacao_a_remov " href="{{ route('empresa.visualizar', ['id' => $empresa->id]) }}">
-                <div class="card-empresa mb-3 ">
+
+                <div class="card-empresa">
                     <div class="row g-0">
                         <div class="col-md-4">
                             <img src="{{ Voyager::image($empresa->logo) }}" class=" foto_empresa" alt="Logo Empresa">
                         </div>
                         <div class="col-md-8">
                             <div class="titulo_empresa">
-                                <h5 class="nome_empresa">{{ $empresa->nome }}</h5>
-                                <h6 class="categoria_empresa">{{ $empresa->categoria->nome }}</h6>
-                                <p class="localizacao-empre">11km</p>
+                                <h5 class="nome_empresa text-white">{{ $empresa->nome }}</h5>
+                                <h6 class="categoria_empresa text-white">{{ $empresa->categoria->nome }}</h6>
+                                <p class="localizacao-empre text-white">11km</p>
+                                <a type="button" class="btn btn-danger botao_conferir_produto marcacao_a_remov" href="{{ route('empresa.visualizar', ['id' => $empresa->id]) }}"><i class="fa-sharp fa-solid fa-shop"></i> Confira os produtos
+                                </a>
 
                             </div>
                         </div>
                     </div>
                 </div>
-            </a>
+
         @empty
             <p>Nenhuma empresa cadastrada</p>
         @endforelse
