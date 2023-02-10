7f9767b46c93906234294896a9ceef658de71d5d
João Marcelo Cassavara <joao.cassavara@estudante.ifms.edu.br>
2023-02-09 23:49:30 -0400

    Visualizar avaliação empresa

--- resources/views/empresa/visualizar.blade.php
+++ resources/views/empresa/visualizar.blade.php
@@ -273,7 +273,9 @@
             @endforelse
         </section>
     @endforeach
-    <header class="py-3 ps-5"> <h1 class="fw-bold">Avaliação da empresa</h1></header>
+    <header class="py-5 ps-5">
+        <h1 class="fw-bold">Avaliação da empresa</h1>
+    </header>
     @forelse ($empresa->avaliacoes as $avaliacao)
         <div class="card_avalicao_empresa rounded-3">
             <div class="d-flex justify-content-around  m-4 p-2">
@@ -286,45 +288,35 @@
                         <p>{{ $avaliacao->usuario->name }}</p>
                         {{-- <p>{{ $avaliacao->avaliacao }}</p> --}}
                         <fieldset class="rating">
-                            <input type="radio" id="star5" name="rating"
-                                value="5" /><label class="full" for="star5"
-                                title="Awesome - 5 stars"></label>
-                            <input type="radio" id="star4half" name="rating"
-                                value="4 and a half" /><label class="half" for="star4half"
-                                title="Pretty good - 4.5 stars"></label>
-                            <input type="radio" id="star4" name="rating"
-                                value="4" /><label class="full" for="star4"
-                                title="Pretty good - 4 stars"></label>
-                            <input type="radio" id="star3half" name="rating"
-                                value="3 and a half" /><label class="half" for="star3half"
-                                title="Meh - 3.5 stars"></label>
-                            <input type="radio" id="star3" name="rating"
-                                value="3" /><label class="full" for="star3"
-                                title="Meh - 3 stars"></label>
-                            <input type="radio" id="star2half" name="rating"
-                                value="2 and a half" /><label class="half" for="star2half"
-                                title="Kinda bad - 2.5 stars"></label>
-                            <input type="radio" id="star2" name="rating"
-                                value="2" /><label class="full" for="star2"
-                                title="Kinda bad - 2 stars"></label>
-                            <input type="radio" id="star1half" name="rating"
-                                value="1 and a half" /><label class="half" for="star1half"
-                                title="Meh - 1.5 stars"></label>
-                            <input type="radio" id="star1" name="rating"
-                                value="1" /><label class="full" for="star1"
-                                title="Sucks big time - 1 star"></label>
-                            <input type="radio" id="starhalf" name="rating"
-                                value="half" /><label class="half" for="starhalf"
-                                title="Sucks big time - 0.5 stars"></label>
+                            <input type="radio" id="star5" name="rating" value="5" /><label class="full"
+                                for="star5" title="Awesome - 5 stars"></label>
+                            <input type="radio" id="star4half" name="rating" value="4 and a half" /><label
+                                class="half" for="star4half" title="Pretty good - 4.5 stars"></label>
+                            <input type="radio" id="star4" name="rating" value="4" /><label class="full"
+                                for="star4" title="Pretty good - 4 stars"></label>
+                            <input type="radio" id="star3half" name="rating" value="3 and a half" /><label
+                                class="half" for="star3half" title="Meh - 3.5 stars"></label>
+                            <input type="radio" id="star3" name="rating" value="3" /><label class="full"
+                                for="star3" title="Meh - 3 stars"></label>
+                            <input type="radio" id="star2half" name="rating" value="2 and a half" /><label
+                                class="half" for="star2half" title="Kinda bad - 2.5 stars"></label>
+                            <input type="radio" id="star2" name="rating" value="2" /><label class="full"
+                                for="star2" title="Kinda bad - 2 stars"></label>
+                            <input type="radio" id="star1half" name="rating" value="1 and a half" /><label
+                                class="half" for="star1half" title="Meh - 1.5 stars"></label>
+                            <input type="radio" id="star1" name="rating" value="1" /><label class="full"
+                                for="star1" title="Sucks big time - 1 star"></label>
+                            <input type="radio" id="starhalf" name="rating" value="half" /><label class="half"
+                                for="starhalf" title="Sucks big time - 0.5 stars"></label>
                         </fieldset>
 
-                        <p>Postado: {{ $avaliacao->updated_at }}</p>
+                        <p>{{ $avaliacao->updated_at }}</p>
 
                     </div>
 
                 </div>
 
-                <div class="d-flex align-items-center w-50">
+                <div class="d-flex align-items-center w-50 p-3">
 
                     <p class="text-break">{{ $avaliacao->descricao }}</p>
 
