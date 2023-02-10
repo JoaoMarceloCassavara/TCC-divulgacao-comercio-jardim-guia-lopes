359b9701e1498f17f49072336ba09a0b8229709b
João Marcelo Cassavara <joao.cassavara@estudante.ifms.edu.br>
2023-02-10 00:45:06 -0400

    modal_produro

--- resources/views/produto/index.blade.php
+++ resources/views/produto/index.blade.php
@@ -10,7 +10,8 @@
             <a class="marcacao_a_remov text-black" href="{{ route('produto.categoria', ['id' => $categoria_produto->id]) }}">
                 <div class="card_categoria_produto rounded-3 p-5 m-2">
                     <img src="{{ Voyager::image($categoria_produto->imagem) }}" width="120" height="95" class="rounded-3"
-                        alt="Imagem Da categoria Dos Produtos" onerror="this.onerror=null;this.src='{{ asset('assets/images/exemplos/4.jpg') }}';">
+                        alt="Imagem Da categoria Dos Produtos"
+                        onerror="this.onerror=null;this.src='{{ asset('assets/images/exemplos/4.jpg') }}';">
                     <div class="text-center py-2">
                         <p class="fs-5 card-title">{{ $categoria_produto->nome }}</p>
                     </div>
@@ -32,12 +33,13 @@
                 <div class="card-link m-2 p-4">
 
                     <img src="{{ Voyager::image($produto->imagem) }}" width="210" height="185" class="imagem-produto"
-                        alt="Imagem do produto" onerror="this.onerror=null;this.src='{{ asset('assets/images/exemplos/4.jpg') }}';">
+                        alt="Imagem do produto"
+                        onerror="this.onerror=null;this.src='{{ asset('assets/images/exemplos/4.jpg') }}';">
 
                     <div class="card-componente">
                         <h5 class="card-title">{{ $produto->nome }}</h5>
                         <p class="card-text">{{ $produto->descricao }}</p>
-                        <p class="card-text">Preço {{ $produto->getPreco()}} </p>
+                        <p class="card-text">Preço {{ $produto->getPreco() }} </p>
                     </div>
                 </div>
             </a>
@@ -53,61 +55,59 @@
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
+                            <div class="d-flex ps-5 ">
+                                <div class="modal-card-produto p-4 rounded-3">
+                                    <img src="{{ Voyager::image($produto->imagem) }}" width="160" height="140"
+                                        class="rounded-3" alt="Imagem do produto"
+                                        onerror="this.onerror=null;this.src='{{ asset('assets/images/exemplos/4.jpg') }}';">
+                                </div>
 
-                            <section class=" d-flex flex-wrap index_padding">
-                                <div class="col-md-4">
-                                    <div class="card-modal shadow p-4 m-2 ">
-                                        <img src="{{ Voyager::image($produto->imagem) }}" class="modal-imagem"
-                                            class="imagem-produto"  alt="Imagem do produto"onerror="this.onerror=null;this.src='{{ asset('assets/images/exemplos/4.jpg') }}';">
+                                <div class="ps-5">
+
+                                    <h5 class="">{{ $produto->nome }}</h5>
+                                    <fieldset class="rating">
+                                        <input type="radio" id="star5" name="rating"
+                                            value="5" /><label class="full" for="star5"
+                                            title="Awesome - 5 stars"></label>
+                                        <input type="radio" id="star4half" name="rating"
+                                            value="4 and a half" /><label class="half" for="star4half"
+                                            title="Pretty good - 4.5 stars"></label>
+                                        <input type="radio" id="star4" name="rating"
+                                            value="4" /><label class="full" for="star4"
+                                            title="Pretty good - 4 stars"></label>
+                                        <input type="radio" id="star3half" name="rating"
+                                            value="3 and a half" /><label class="half" for="star3half"
+                                            title="Meh - 3.5 stars"></label>
+                                        <input type="radio" id="star3" name="rating"
+                                            value="3" /><label class="full" for="star3"
+                                            title="Meh - 3 stars"></label>
+                                        <input type="radio" id="star2half" name="rating"
+                                            value="2 and a half" /><label class="half" for="star2half"
+                                            title="Kinda bad - 2.5 stars"></label>
+                                        <input type="radio" id="star2" name="rating"
+                                            value="2" /><label class="full" for="star2"
+                                            title="Kinda bad - 2 stars"></label>
+                                        <input type="radio" id="star1half" name="rating"
+                                            value="1 and a half" /><label class="half" for="star1half"
+                                            title="Meh - 1.5 stars"></label>
+                                        <input type="radio" id="star1" name="rating"
+                                            value="1" /><label class="full" for="star1"
+                                            title="Sucks big time - 1 star"></label>
+                                        <input type="radio" id="starhalf" name="rating"
+                                            value="half" /><label class="half" for="starhalf"
+                                            title="Sucks big time - 0.5 stars"></label>
+                                    </fieldset>
+                                    <p>Pedidos</p>
+
+                                    <div class="py-2">
+                                        <p>Avaliações do produto</p>
+
+
                                     </div>
                                 </div>
 
-                                <div class="col-md-8">
-                                    <div class="card-text-modal ">
-                                        <h5 class="card-title modal-font-produto">{{ $produto->nome }}</h5>
 
-
-                                        <fieldset class="rating">
-                                            <input type="radio" id="star5" name="rating" value="5" /><label
-                                                class="full" for="star5" title="Awesome - 5 stars"></label>
-                                            <input type="radio" id="star4half" name="rating"
-                                                value="4 and a half" /><label class="half" for="star4half"
-                                                title="Pretty good - 4.5 stars"></label>
-                                            <input type="radio" id="star4" name="rating" value="4" /><label
-                                                class="full" for="star4" title="Pretty good - 4 stars"></label>
-                                            <input type="radio" id="star3half" name="rating"
-                                                value="3 and a half" /><label class="half" for="star3half"
-                                                title="Meh - 3.5 stars"></label>
-                                            <input type="radio" id="star3" name="rating" value="3" /><label
-                                                class="full" for="star3" title="Meh - 3 stars"></label>
-                                            <input type="radio" id="star2half" name="rating"
-                                                value="2 and a half" /><label class="half" for="star2half"
-                                                title="Kinda bad - 2.5 stars"></label>
-                                            <input type="radio" id="star2" name="rating" value="2" /><label
-                                                class="full" for="star2" title="Kinda bad - 2 stars"></label>
-                                            <input type="radio" id="star1half" name="rating"
-                                                value="1 and a half" /><label class="half" for="star1half"
-                                                title="Meh - 1.5 stars"></label>
-                                            <input type="radio" id="star1" name="rating" value="1" /><label
-                                                class="full" for="star1" title="Sucks big time - 1 star"></label>
-                                            <input type="radio" id="starhalf" name="rating" value="half" /><label
-                                                class="half" for="starhalf" title="Sucks big time - 0.5 stars"></label>
-                                        </fieldset>
-
-                                        <p class="paragrafo-pedido">65 pedidos</p>
-                                        <div class="input-ajuste">
-
-                                            <h4>Alguma observação?</h4>
-                                            <div class="input-group input-group-lg input-campo-modal ">
-
-                                                <input type="text" class="form-control"placeholder="Ajuste"
-                                                    aria-label="Sizing example input"
-                                                    aria-describedby="inputGroup-sizing-lg">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </section>
+                            </div>
 
 
                         </div>
@@ -117,6 +117,59 @@
                                 data-imagem="{{ Voyager::image($produto->imagem) }}" data-preco="{{ $produto->preco }}"
                                 data-nome="{{ $produto->nome }}" onclick="adicionarItemNoCarrinho();">Adicionar ao
                                 carrinho</button>
+                        </div>
+                        <div class="">
+                        <p>avaliações</p>
+                        @forelse ($produto->avaliacoes as $avaliacao)
+                        <div class="card_avaliacao_produto rounded-3">
+                            <div class="d-flex justify-content-around  m-1 p-2">
+                                <div class="d-flex">
+                                    <img src="{{ Voyager::image($avaliacao->usuario->avatar) }}" width="160" height="140" alt="Imagem do produto"
+                                        onerror="this.onerror=null;this.src='{{ asset('assets/images/exemplos/4.jpg') }}';">
+
+                                    <div class="ps-3 py-5">
+                                        <p>{{ $avaliacao->usuario->name }}</p>
+                                        {{-- <p>{{ $avaliacao->avaliacao }}</p> --}}
+                                        <fieldset class="rating">
+                                            <input type="radio" id="star5" name="rating" value="5" /><label class="full"
+                                                for="star5" title="Awesome - 5 stars"></label>
+                                            <input type="radio" id="star4half" name="rating" value="4 and a half" /><label
+                                                class="half" for="star4half" title="Pretty good - 4.5 stars"></label>
+                                            <input type="radio" id="star4" name="rating" value="4" /><label class="full"
+                                                for="star4" title="Pretty good - 4 stars"></label>
+                                            <input type="radio" id="star3half" name="rating" value="3 and a half" /><label
+                                                class="half" for="star3half" title="Meh - 3.5 stars"></label>
+                                            <input type="radio" id="star3" name="rating" value="3" /><label class="full"
+                                                for="star3" title="Meh - 3 stars"></label>
+                                            <input type="radio" id="star2half" name="rating" value="2 and a half" /><label
+                                                class="half" for="star2half" title="Kinda bad - 2.5 stars"></label>
+                                            <input type="radio" id="star2" name="rating" value="2" /><label class="full"
+                                                for="star2" title="Kinda bad - 2 stars"></label>
+                                            <input type="radio" id="star1half" name="rating" value="1 and a half" /><label
+                                                class="half" for="star1half" title="Meh - 1.5 stars"></label>
+                                            <input type="radio" id="star1" name="rating" value="1" /><label class="full"
+                                                for="star1" title="Sucks big time - 1 star"></label>
+                                            <input type="radio" id="starhalf" name="rating" value="half" /><label class="half"
+                                                for="starhalf" title="Sucks big time - 0.5 stars"></label>
+                                        </fieldset>
+
+                                        <p>{{ $avaliacao->updated_at }}</p>
+
+                                    </div>
+
+                                </div>
+
+                                <div class="d-flex align-items-center w-50 p-3">
+
+                                    <p class="text-break">{{ $avaliacao->descricao }}</p>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+                    @empty
+                    @endforelse
                         </div>
                     </div>
                 </div>
