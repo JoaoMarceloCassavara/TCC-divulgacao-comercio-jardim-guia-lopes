d747047cb83e29562baa2366b09c9a8707a364cb
João Marcelo Cassavara <joao.cassavara@estudante.ifms.edu.br>
2023-02-16 10:53:44 -0400

    css

--- resources/views/busca.blade.php
+++ resources/views/busca.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('conteudo')
-<header class="pt-5">
+<header class="py-4">
     <h1 class="fw-bold">Buscando por <span class="text-danger">
             {{ request()->filtro }}
             @empty(request()->filtro)
@@ -12,11 +12,11 @@
     <!-- Tabs navs -->
     <ul class="nav nav-tabs mb-3" id="ex1" role="tablist">
         <li class="nav-item" role="presentation">
-            <a class="nav-link active" id="ex1-tab-1" data-mdb-toggle="tab" href="#ex1-tabs-1" role="tab"
-                aria-controls="ex1-tabs-1" aria-selected="true">Produtos</a>
+            <a class="nav-link active text-success" id="ex1-tab-1" data-mdb-toggle="tab" href="#ex1-tabs-1" role="tab"
+                aria-controls="ex1-tabs-1" aria-selected="true">Produtores</a>
         </li>
         <li class="nav-item" role="presentation">
-            <a class="nav-link" id="ex1-tab-2" data-mdb-toggle="tab" href="#ex1-tabs-2" role="tab"
+            <a class="nav-link text-success" id="ex1-tab-2" data-mdb-toggle="tab" href="#ex1-tabs-2" role="tab"
                 aria-controls="ex1-tabs-2" aria-selected="false">Empresas</a>
         </li>
     </ul>
@@ -39,8 +39,8 @@
                         {{-- Final botao Modal --}}>
                         <div class="card-link m-2 p-4">
 
-                            <img src="{{ Voyager::image($produto->imagem) }}" width="210" height="185"
-                                class="imagem-produto" alt="Imagem do produto">
+                            <img src="{{ Voyager::image($produto->imagem) }}" width="200" height="185" class="rounded-2"
+                             alt="Imagem do produto">
 
                             <div class="card-componente">
                                 <h5 class="card-title">{{ $produto->nome }}</h5>
@@ -157,20 +157,52 @@
             </header> --}}
             <section class=" d-flex flex-wrap py-5">
                 @forelse ($empresas as $empresa)
-                    <div class="card_empresa d-flex p-4 m-2 ">
-                        <img src="{{ Voyager::image($empresa->logo) }}"height="160" width="160"
-                            class="rounded-circle"
-                            alt="Logo Empresa"onerror="this.onerror=null;this.src='{{ asset('assets/images/exemplos/4.jpg') }}';">
-                        <div class="col-md-8 ">
-                            <h5 class="text-white text-center nome_empresa py-2">{{ $empresa->nome }}</h5>
-                            <h6 class=" text-white ps-5">{{ $empresa?->categoria?->nome }}</h6>
-                            <p class=" text-white ps-5">11km</p>
-                            <a type="button" class="btn btn-danger botao_conferir_produto marcacao_a_remov m-3"
-                                href="{{ route('empresa.visualizar', ['id' => $empresa->id]) }}"><i
-                                    class="fa-sharp fa-solid fa-shop"></i> Confira os produtos
-                            </a>
-                        </div>
+                <div class="card_empresa d-flex align-items-center  m-3 p-4">
+                    <div>
+                    <img src="{{ Voyager::image($empresa->logo) }}"height="130" width="130" class="rounded-circle"
+                        alt="Logo Empresa"onerror="this.onerror=null;this.src='{{ asset('assets/images/produto_padrao.jpg') }}';">
                     </div>
+                    <div class="ps-4">
+                        <p class="text-white  fs-5 pt-4 ">{{ $empresa->nome }}</p>
+                        <h6 class=" text-white">{{ $empresa?->categoria?->nome }}</h6>
+                       <fieldset class="rating py-2">
+                            <input type="radio" id="star5" name="rating"
+                                value="5" /><label class="full" for="star5"
+                                title="Awesome - 5 stars"></label>
+                            <input type="radio" id="star4half" name="rating"
+                                value="4 and a half" /><label class="half" for="star4half"
+                                title="Pretty good - 4.5 stars"></label>
+                            <input type="radio" id="star4" name="rating"
+                                value="4" /><label class="full" for="star4"
+                                title="Pretty good - 4 stars"></label>
+                            <input type="radio" id="star3half" name="rating"
+                                value="3 and a half" /><label class="half" for="star3half"
+                                title="Meh - 3.5 stars"></label>
+                            <input type="radio" id="star3" name="rating"
+                                value="3" /><label class="full" for="star3"
+                                title="Meh - 3 stars"></label>
+                            <input type="radio" id="star2half" name="rating"
+                                value="2 and a half" /><label class="half" for="star2half"
+                                title="Kinda bad - 2.5 stars"></label>
+                            <input type="radio" id="star2" name="rating"
+                                value="2" /><label class="full" for="star2"
+                                title="Kinda bad - 2 stars"></label>
+                            <input type="radio" id="star1half" name="rating"
+                                value="1 and a half" /><label class="half" for="star1half"
+                                title="Meh - 1.5 stars"></label>
+                            <input type="radio" id="star1" name="rating"
+                                value="1" /><label class="full" for="star1"
+                                title="Sucks big time - 1 star"></label>
+                            <input type="radio" id="starhalf" name="rating"
+                                value="half" /><label class="half" for="starhalf"
+                                title="Sucks big time - 0.5 stars"></label>
+                        </fieldset>
+                        <a type="button" class="btn btn-danger botao_conferir_produto marcacao_a_remov"
+                            href="{{ route('empresa.visualizar', ['id' => $empresa->id]) }}"><i
+                                class="fa-sharp fa-solid fa-shop"></i> Confira os produtos
+                        </a>
+                    </div>
+                </div>
                 @empty
                     <p class="alert alert-success">Não encontrado nenhuma Empresa com esse nome: {{ request()->filtro }}
                     </p>
