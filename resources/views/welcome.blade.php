dc241db5c82d20b9d8e551b09abbdc10bc5b4613
João Marcelo Cassavara <joao.cassavara@estudante.ifms.edu.br>
2023-02-28 17:10:26 -0400

    eestrelas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,39 +18,10 @@
             <div class="ps-4">
                 <p class="text-white  fs-5 pt-4 ">{{ $empresa->nome }}</p>
                 <h6 class=" text-white">{{ $empresa?->categoria?->nome }}</h6>
-                <input class="rating py-2" type="range" value="{{$empresa->avaliacao}}" disabled>
-               <fieldset class="rating py-2">
-                    <input type="radio" id="star5" name="rating"
-                        value="5" /><label class="full" for="star5"
-                        title="Awesome - 5 stars"></label>
-                    <input type="radio" id="star4half" name="rating"
-                        value="4 and a half" /><label class="half" for="star4half"
-                        title="Pretty good - 4.5 stars"></label>
-                    <input type="radio" id="star4" name="rating"
-                        value="4" /><label class="full" for="star4"
-                        title="Pretty good - 4 stars"></label>
-                    <input type="radio" id="star3half" name="rating"
-                        value="3 and a half" /><label class="half" for="star3half"
-                        title="Meh - 3.5 stars"></label>
-                    <input type="radio" id="star3" name="rating"
-                        value="3" /><label class="full" for="star3"
-                        title="Meh - 3 stars"></label>
-                    <input type="radio" id="star2half" name="rating"
-                        value="2 and a half" /><label class="half" for="star2half"
-                        title="Kinda bad - 2.5 stars"></label>
-                    <input type="radio" id="star2" name="rating"
-                        value="2" /><label class="full" for="star2"
-                        title="Kinda bad - 2 stars"></label>
-                    <input type="radio" id="star1half" name="rating"
-                        value="1 and a half" /><label class="half" for="star1half"
-                        title="Meh - 1.5 stars"></label>
-                    <input type="radio" id="star1" name="rating"
-                        value="1" /><label class="full" for="star1"
-                        title="Sucks big time - 1 star"></label>
-                    <input type="radio" id="starhalf" name="rating"
-                        value="half" /><label class="half" for="starhalf"
-                        title="Sucks big time - 0.5 stars"></label>
-                </fieldset>
+                {{-- <input class="rating py-2" type="range" value="{{$empresa->avaliacao}}" disabled> --}}
+                <label for="avaliacao" class="rating-label">
+                    <input class="rating rating--nojs" id="avaliacao" name="avaliacao" type="range" max="5" step="0.5" value="{{$empresa->avaliacao}}" disabled>
+                </label>
                 <a type="button" class="btn btn-danger botao_conferir_produto marcacao_a_remov"
                     href="{{ route('empresa.visualizar', ['id' => $empresa->id]) }}"><i
                         class="fa-sharp fa-solid fa-shop"></i> Confira os produtos
@@ -94,39 +65,10 @@
                 <div class="ps-4">
                     <p class="text-white  fs-5 pt-4 ">{{ $empresa->nome }}</p>
                     <h6 class=" text-white">{{ $empresa?->categoria?->nome }}</h6>
-                    <input class="rating py-2" type="range" value="{{$empresa->avaliacao}}" disabled>
-                   <fieldset class="rating py-2">
-                        <input type="radio" id="star5" name="rating"
-                            value="5" /><label class="full" for="star5"
-                            title="Awesome - 5 stars"></label>
-                        <input type="radio" id="star4half" name="rating"
-                            value="4 and a half" /><label class="half" for="star4half"
-                            title="Pretty good - 4.5 stars"></label>
-                        <input type="radio" id="star4" name="rating"
-                            value="4" /><label class="full" for="star4"
-                            title="Pretty good - 4 stars"></label>
-                        <input type="radio" id="star3half" name="rating"
-                            value="3 and a half" /><label class="half" for="star3half"
-                            title="Meh - 3.5 stars"></label>
-                        <input type="radio" id="star3" name="rating"
-                            value="3" /><label class="full" for="star3"
-                            title="Meh - 3 stars"></label>
-                        <input type="radio" id="star2half" name="rating"
-                            value="2 and a half" /><label class="half" for="star2half"
-                            title="Kinda bad - 2.5 stars"></label>
-                        <input type="radio" id="star2" name="rating"
-                            value="2" /><label class="full" for="star2"
-                            title="Kinda bad - 2 stars"></label>
-                        <input type="radio" id="star1half" name="rating"
-                            value="1 and a half" /><label class="half" for="star1half"
-                            title="Meh - 1.5 stars"></label>
-                        <input type="radio" id="star1" name="rating"
-                            value="1" /><label class="full" for="star1"
-                            title="Sucks big time - 1 star"></label>
-                        <input type="radio" id="starhalf" name="rating"
-                            value="half" /><label class="half" for="starhalf"
-                            title="Sucks big time - 0.5 stars"></label>
-                    </fieldset>
+                    {{-- <input class="rating py-2" type="range" value="{{$empresa->avaliacao}}" disabled> --}}
+                    <label for="avaliacao" class="rating-label">
+                        <input class="rating rating--nojs" id="avaliacao" name="avaliacao" type="range" max="5" step="0.5" value="{{$empresa->avaliacao}}" disabled>
+                    </label>
                     <a type="button" class="btn btn-danger botao_conferir_produto marcacao_a_remov"
                         href="{{ route('empresa.visualizar', ['id' => $empresa->id]) }}"><i
                             class="fa-sharp fa-solid fa-shop"></i> Confira os produtos
