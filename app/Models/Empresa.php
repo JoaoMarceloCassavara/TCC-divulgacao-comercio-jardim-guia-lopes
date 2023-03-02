fcec39d910819935a82292ec9001a26d487f6058
João Marcelo Cassavara <joao.cassavara@estudante.ifms.edu.br>
2023-03-02 15:28:06 -0400

    atendente xadastrado e funcionando

--- app/Models/Empresa.php
+++ app/Models/Empresa.php
@@ -21,6 +21,9 @@ class Empresa extends Model
         if ($user->hasRole('admin')) {
             return $query;
         }
+        if ($user->hasRole('atendente')) {
+            return $query;
+        }
         return $query->where('user_id', $user->getKey());
     }
 /**
--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -22,6 +22,9 @@ class Produto extends Model
         if ($user->hasRole('admin')) {
             return $query;
         }
+        if ($user->hasRole('atendente')) {
+            return $query;
+        }
         return $query->where('user_id', $user->getKey());
     }
 
