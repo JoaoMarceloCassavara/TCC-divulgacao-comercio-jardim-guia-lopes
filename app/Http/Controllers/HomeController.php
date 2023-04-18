6ad2e2820dc8317e7094df26a330e02eb7a87a91
João Marcelo Cassavara <joao.cassavara@estudante.ifms.edu.br>
2023-04-18 16:20:03 -0400

    usuario empresario sem empresa redirect

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Empresa;
 
 class HomeController extends Controller
 {
@@ -21,9 +24,20 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
+    // public function index()
+    // {
+    //     // return redirect()->route('voyager.dashboard');
+    //     return redirect()->route('welcome');
+    // }
     public function index()
     {
         // return redirect()->route('voyager.dashboard');
+        $tipoUsuarioLogado = Auth::user()->role()->first();
+        $quantidadeDeEmpresas = Empresa::where('user_id',Auth::user()->id)->count();
+        // autorização para verificar o tipo de usuario
+        if ($tipoUsuarioLogado->name == 'empresario'){
+            return redirect()->route('empresa.cadastrar');
+        }
         return redirect()->route('welcome');
     }
 }
