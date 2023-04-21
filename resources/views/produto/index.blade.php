aec917351cecbc8b1c43629705e348b18f81d269
João Marcelo Cassavara <joao.cassavara@estudante.ifms.edu.br>
2023-04-20 22:46:08 -0400

    modal produto

--- resources/views/produto/index.blade.php
+++ resources/views/produto/index.blade.php
@@ -83,19 +83,20 @@
                                 data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                            <div class="d-flex ps-5 ">
-                                <div class="modal-card-produto p-4 rounded-3">
-                                    <img src="{{ Voyager::image($produto->imagem) }}" width="170" height="160"
-                                        class="rounded-3" alt="Imagem do produto"
-                                        onerror="this.onerror=null;this.src='{{ asset('assets/images/imagens-default/foto-do-produto.png') }}';">
+                            <div class="row mb-3">
+                                <div class="col-sm-4">
+                                    <div class="d-flex justify-content-center">
+                                        <div class="modal-card-produto p-4 rounded-3 ">
+                                            <img src="{{ Voyager::image($produto->imagem) }}" class="rounded-3 img-fluid"
+                                                width="190" height="180" alt="Imagem do produto"
+                                                onerror="this.onerror=null;this.src='{{ asset('assets/images/imagens-default/foto-do-produto.png') }}';">
+                                        </div>
+                                    </div>
                                 </div>
-
-                                <div class="ps-4">
-
-                                    <p class=" fw-bold fs-3">{{ $produto->nome }}</p>
-                                    <div class="d-flex">
-                                        {{-- <input class="rating py-2" type="range" value="{{$produto->avaliacao}}" disabled> --}}
-                                        <label for="avaliacao" class="rating-label">
+                                <div class="col-sm-8">
+                                    <p class="fw-bold fs-3">{{ $produto->nome }}</p>
+                                    <div class="d-flex align-items-center">
+                                        <label for="avaliacao" class="rating-label me-2">
                                             <input class="rating rating--nojs" id="avaliacao" name="avaliacao"
                                                 type="range" max="5" step="0.5"
                                                 value="{{ $produto->avaliacao ?? 0 }}" disabled>
@@ -105,36 +106,26 @@
                                                 <p class="text-avaliacao-modal fw-bold">
                                                     {{ number_format($produto->avaliacao, 1, '.', '') }}</p>
                                             @endisset
-
                                         @empty($produto->avaliacao)
-
                                             <h5 class="text-avaliacao-empresa">0.0</h5>
-
                                         @endempty
                                     </div>
                                     <div>
                                         <p class="text-decoration-underline text-success">
-                                            ({{ $produto->avaliacoes->count() }} avaliações de clientes)
-                                        </p>
+                                            ({{ $produto->avaliacoes->count() }} avaliações de clientes)</p>
                                     </div>
                                 </div>
                                 <div class="py-2">
-                                    <p class="fw-bold fs-6">Pedidos {{ $produto->pedidos->count() }}</p>
-                                    <p class="pt-1 fw-bold fs-5">Preço {{ $produto->getPreco() }}
+                                    <p class="fw-bold fs-6 mb-1">Pedidos {{ $produto->pedidos->count() }}</p>
+                                    <p class="fw-bold fs-5">Preço {{ $produto->getPreco() }}</p>
                                 </div>
                             </div>
-
-
                         </div>
-                        <div class="d-flex">
-                            <div class="pt-5 ps-4">
-                                <script src="https://www.youtube.com/iframe_api"></script>
-                                <script src="https://player.vimeo.com/api/player.js"></script>
-
-
-
-                                @isset($produto->video_curto)
-                                    {{-- Rodar o video do youtube  --}}
+                        <div class="row">
+                            @isset($produto->video_curto)
+                                <div class="col-md-6">
+                                    <script src="https://www.youtube.com/iframe_api"></script>
+                                    <script src="https://player.vimeo.com/api/player.js"></script>
                                     @php
                                         $videoUrl = $produto->video_curto;
                                         $videoId = '';
@@ -150,91 +141,93 @@
                                     @endphp
                                     @if (!empty($videoId))
                                         <div class="embed-responsive embed-responsive-16by9">
-                                            <iframe id="video-player" width="450" height="315"
-                                                class="embed-responsive-item"
+                                            <iframe id="video-player" class="embed-responsive-item w-100 h-100"
                                                 src="https://www.youtube.com/embed/{{ $videoId }}?enablejsapi=1"
                                                 frameborder="0" allowfullscreen></iframe>
                                         </div>
                                     @endif
-                                    {{-- Rodar o video do youtube Fim --}}
-                                    <!-- Seção de scripts -->
-                                @endisset
-
-
+                                </div>
+                                <div class="col-md-6">
+                                @else
+                                    <div class="col-md-12 ps-5">
+                                    @endisset
+                                    <h4>Descrição</h4>
+                                    <p style="max-height: 120px; overflow-y: auto;">{{ $produto->descricao }}</p>
+                                </div>
                             </div>
-                            <div class="pt-5 ps-3">
-                                <h4>Descrição</h4>
-                                <p>{{ $produto->descricao }}</p>
+                        </div>
 
-                            </div>
+                        <div class="modal-footer">
+                            <a type="button"  href="{{ route('empresa.visualizar', ['id' => $produto->empresa->id]) }}" class="btn btn-primary">Acessar Página do <br> Produtor: {{$produto->empresa->nome}}</a>
+                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Voltar</button>
+                            <button type="button" class="btn btn-success" data-id="{{ $produto->id }}"
+                                data-imagem="{{ Voyager::image($produto->imagem) }}"
+                                data-preco="{{ $produto->preco }}" data-nome="{{ $produto->nome }}"
+                                onclick="adicionarItemNoCarrinho();">Adicionar ao
+                                carrinho</button>
+                               
 
                         </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Voltar</button>
-                        <button type="button" class="btn btn-dark " data-id="{{ $produto->id }}"
-                            data-imagem="{{ Voyager::image($produto->imagem) }}" data-preco="{{ $produto->preco }}"
-                            data-nome="{{ $produto->nome }}" onclick="adicionarItemNoCarrinho();">Adicionar ao
-                            carrinho</button>
-                    </div>
-                    <hr class="border border-dark ">
+                        <hr class="border border-dark ">
 
-                    <header class="ps-3 py-4">
-                        <p class="fw-bold fs-4">Avaliações</p>
-                    </header>
-                    @forelse ($produto->avaliacoes as $avaliacao)
-                        <div class="rounded-3 shadow ms-3 me-3 mb-5 bg-body rounded">
-                            <div class="d-flex p-2 bd-highlight mb-3 m-3">
-                                <div class="d-flex align-items-center">
-                                    <div class="pt-5 flex-shrink-0">
-                                        <img src="{{ Voyager::image($avaliacao->usuario->avatar) }}" width="140"
-                                            height="120" alt="Imagem avatar" class="rounded-circle"
-                                            onerror="this.onerror=null;this.src='{{ asset('assets/images/imagens-default/user_default.png') }}';">
-                                        <div class="ps-2 pt-3">
-                                            <label for="avaliacao" class="rating-label">
-                                                <input class="rating rating--nojs" id="avaliacao" name="avaliacao"
-                                                    type="range" max="5" step="0.5"
-                                                    value="{{ $avaliacao->avaliacao ?? 0 }}" disabled>
-                                            </label>
+                        <header class="ps-3 py-4">
+                            <p class="fw-bold fs-4">Avaliações</p>
+                        </header>
+                        @forelse ($produto->avaliacoes as $avaliacao)
+                            <div class="rounded-3 shadow ms-3 me-3 mb-5 bg-body rounded">
+                                <div class="d-flex p-2 bd-highlight mb-3 m-3">
+                                    <div class="d-flex align-items-center">
+                                        <div class="pt-5 flex-shrink-0">
+                                            <img src="{{ Voyager::image($avaliacao->usuario->avatar) }}"
+                                                width="140" height="120" alt="Imagem avatar"
+                                                class="rounded-circle"
+                                                onerror="this.onerror=null;this.src='{{ asset('assets/images/imagens-default/user_default.png') }}';">
+                                            <div class="ps-2 pt-3">
+                                                <label for="avaliacao" class="rating-label">
+                                                    <input class="rating rating--nojs" id="avaliacao"
+                                                        name="avaliacao" type="range" max="5"
+                                                        step="0.5" value="{{ $avaliacao->avaliacao ?? 0 }}"
+                                                        disabled>
+                                                </label>
+                                            </div>
+                                        </div>
+                                        <div class="ps-3" style="max-width: 200px;">
+                                            <p class="text-break"
+                                                style="font-size: 1.1rem; font-weight: bold; margin-bottom: 0;">
+                                                {{ $avaliacao->usuario->name }}</p>
+                                            <p>{{ $avaliacao->updated_at->format('d/m/Y H:i:s') }}</p>
                                         </div>
                                     </div>
-                                    <div class="ps-3" style="max-width: 200px;">
-                                        <p class="text-break"
-                                            style="font-size: 1.1rem; font-weight: bold; margin-bottom: 0;">
-                                            {{ $avaliacao->usuario->name }}</p>
-                                        <p>{{ $avaliacao->updated_at->format('d/m/Y H:i:s') }}</p>
+                                    <div class="d-flex align-items-start flex-grow-1 p-3 ms-3 mt-3"
+                                        style="max-height: 120px; overflow-y: auto;">
+                                        <p class="text-break mb-0" style="font-size: 1.1rem; max-width: 450px;">
+                                            {{ mb_substr($avaliacao->descricao, 0, 500) }}</p>
                                     </div>
-                                </div>
-                                <div class="d-flex align-items-start flex-grow-1 p-3 ms-3 mt-3"
-                                    style="max-height: 120px; overflow-y: auto;">
-                                    <p class="text-break mb-0" style="font-size: 1.1rem; max-width: 450px;">
-                                        {{ mb_substr($avaliacao->descricao, 0, 500) }}</p>
-                                </div>
 
+                                </div>
                             </div>
-                        </div>
-                    @empty
-                        <div class="ps-4">
-                            <div class="alert alert-success w-25" role="alert">
-                                <p class="text-center">Nenhuma avaliação cadastrada para esse produto.</p>
+                        @empty
+                            <div class="ps-4">
+                                <div class="alert alert-success w-25" role="alert">
+                                    <p class="text-center">Nenhuma avaliação cadastrada para esse produto.</p>
+                                </div>
                             </div>
-                        </div>
-                    @endforelse
+                        @endforelse
 
+                    </div>
                 </div>
             </div>
-        </div>
 
-        {{-- Final MOdal --}}
+            {{-- Final MOdal --}}
 
-    @empty
-        {{-- @if (sizeof($categoria_produtos) == 0) --}}
-        <div class="ps-4">
-            <div class="alert alert-success " role="alert">
-                <p class="text-center">Nenhum produto cadastrado.</p>
+        @empty
+            {{-- @if (sizeof($categoria_produtos) == 0) --}}
+            <div class="ps-4">
+                <div class="alert alert-success " role="alert">
+                    <p class="text-center">Nenhum produto cadastrado.</p>
+                </div>
             </div>
-        </div>
-        {{-- @endif --}}
+            {{-- @endif --}}
     @endforelse
 </section>
 @endsection
