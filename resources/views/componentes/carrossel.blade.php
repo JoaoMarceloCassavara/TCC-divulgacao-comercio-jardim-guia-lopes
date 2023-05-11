00ce84d0c36f41a2613ca878a2f5fdffc25dfc39
João Marcelo Cassavara <joao.cassavara@estudante.ifms.edu.br>
2023-05-11 00:04:02 -0400

    mais 1 item no carrossel de produto e empresa

--- resources/views/componentes/carrossel.blade.php
+++ resources/views/componentes/carrossel.blade.php
@@ -1,7 +1,7 @@
 @if (count($categoria_empresas) > 0)
                 <div id="carousel-categorias-empresas" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
-                        @for ($posicao = 0; $posicao < count($categoria_empresas); $posicao = $posicao + 3)
+                        @for ($posicao = 0; $posicao < count($categoria_empresas); $posicao = $posicao + 4)
                             @if ($posicao == 0)
                                 <div class="carousel-item active">
                             @else
@@ -39,6 +39,18 @@
                                       <img src="{{ Voyager::image($categoria_empresas[$posicao+2]->imagem) }}" width="150" class="rounded-3 img-fluid" alt="Imagem Da categoria Dos Produtos" onerror="this.onerror=null;this.src='{{ asset('assets/images/imagens-default/imagem-categoria.png') }}';">
                                       <div class="text-center pt-2 pb-4">
                                         <p class="fs-5 card-title">{{ $categoria_empresas[$posicao+2]->nome }}</p>
+                                      </div>
+                                    </div>
+                                  </a>
+
+
+                                @endif
+                                @if ($posicao + 3 < count($categoria_empresas))
+                                <a href="{{ route('empresa.categoria', ['id' => $categoria_empresas[$posicao+3]->id]) }}" class="marcacao_a_remov text-black mb-4 mx-3">
+                                    <div class="card_categoria_produto rounded-3 p-3" style="width: 150px; height: 200px;">
+                                      <img src="{{ Voyager::image($categoria_empresas[$posicao+3]->imagem) }}" width="150" class="rounded-3 img-fluid" alt="Imagem Da categoria Dos Produtos" onerror="this.onerror=null;this.src='{{ asset('assets/images/imagens-default/imagem-categoria.png') }}';">
+                                      <div class="text-center pt-2 pb-4">
+                                        <p class="fs-5 card-title">{{ $categoria_empresas[$posicao+3]->nome }}</p>
                                       </div>
                                     </div>
                                   </a>
--- resources/views/componentes/carrossel_categoria_produto.blade.php
+++ resources/views/componentes/carrossel_categoria_produto.blade.php
@@ -1,7 +1,7 @@
 @if (count($categoria_produtos) > 0)
                 <div id="carrossel_categoria_produto" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
-                        @for ($posicao = 0; $posicao < count($categoria_produtos); $posicao = $posicao + 3)
+                        @for ($posicao = 0; $posicao < count($categoria_produtos); $posicao = $posicao + 4)
                             @if ($posicao == 0)
                                 <div class="carousel-item active">
                             @else
@@ -39,6 +39,18 @@
                                       <img src="{{ Voyager::image($categoria_produtos[$posicao+2]->imagem) }}" width="150" class="rounded-3 img-fluid" alt="Imagem Da categoria Dos Produtos" onerror="this.onerror=null;this.src='{{ asset('assets/images/imagens-default/imagem-categoria.png') }}';">
                                       <div class="text-center pt-2 pb-4">
                                         <p class="fs-5 card-title">{{ $categoria_produtos[$posicao+2]->nome }}</p>
+                                      </div>
+                                    </div>
+                                  </a>
+
+
+                                @endif
+                                @if ($posicao + 3 < count($categoria_produtos))
+                                <a href="{{ route('produto.categoria', ['id' => $categoria_produtos[$posicao+3]->id]) }}" class="marcacao_a_remov text-black mb-4 mx-3">
+                                    <div class="card_categoria_produto rounded-3 p-3" style="width: 150px; height: 200px;">
+                                      <img src="{{ Voyager::image($categoria_produtos[$posicao+3]->imagem) }}" width="150" class="rounded-3 img-fluid" alt="Imagem Da categoria Dos Produtos" onerror="this.onerror=null;this.src='{{ asset('assets/images/imagens-default/imagem-categoria.png') }}';">
+                                      <div class="text-center pt-2 pb-4">
+                                        <p class="fs-5 card-title">{{ $categoria_produtos[$posicao+3]->nome }}</p>
                                       </div>
                                     </div>
                                   </a>
